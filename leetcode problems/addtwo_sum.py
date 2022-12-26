06523726c58c10b0f0b562a65155c4e6aea5b449
amirkhan1092 <amirkhan1092@gmail.com>
2022-12-26 18:52:11 +0530

    Two Sum Leetcode

---
+++ leetcode problems/addtwo_sum.py
@@ -0,0 +1,24 @@
+'''
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+Input: nums = [3,3], target = 6'''
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+# Path: leetcode problems\addtwo_sum.py
+# main code
+if __name__ == '__main__':
+    nums = [2,7,11,15]
+    target = 9
+    solution = Solution()
+    print(solution.twoSum(nums,target))
